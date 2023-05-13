143e46a93b668c6fa98c3f168a3eaaf811808fcf
Riko <riko.14381438@gmail.com>
2023-05-13 14:20:28 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-About project
 
+  # lens map
 
-Lens Map is a social web application built on the Lens protocol. It enables users to share posts and location-based content on a map. Users can follow or mirror other users' posts to stay informed and discover new content.
-From a business perspective, organizations and cities can use the app to promote their brand and events. For example, an organization could create an NFT badge and place it on the map. Users who collect the badge could be eligible for special benefits or rewards, and the organization could use this as a way to promote their brand and drive engagement with their audience. Overall, Lens Map is a powerful tool for connecting users with location-based content and helping businesses engage with their audience in new and innovative ways.
+  Note: Please ensure you have installed <code><a href="https://nodejs.org/en/download/">nodejs</a></code>
 
+  To preview and run the project on your device:
+  1) Open project folder in <a href="https://code.visualstudio.com/download">Visual Studio Code</a>
+  2) In the terminal, run `npm install`
+  3) Run `npm start` to view project in browser
+  
---
+++ script.js
@@ -0,0 +1,100 @@
+const query = `
+  query Profiles {
+    profiles(request: { ownedBy: ["0xD020E01C0c90Ab005A01482d34B808874345FD82"], limit: 10 }) {
+      items {
+        id
+        name
+        bio
+        attributes {
+          displayType
+          traitType
+          key
+          value
+        }
+        followNftAddress
+        metadata
+        isDefault
+        picture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+          __typename
+        }
+        handle
+        coverPicture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+          __typename
+        }
+        ownedBy
+        dispatcher {
+          address
+          canUseRelay
+        }
+        stats {
+          totalFollowers
+          totalFollowing
+          totalPosts
+          totalComments
+          totalMirrors
+          totalPublications
+          totalCollects
+        }
+        followModule {
+          ... on FeeFollowModuleSettings {
+            type
+            amount {
+              asset {
+                symbol
+                name
+                decimals
+                address
+              }
+              value
+            }
+            recipient
+          }
+          ... on ProfileFollowModuleSettings {
+           type
+          }
+          ... on RevertFollowModuleSettings {
+           type
+          }
+        }
+      }
+      pageInfo {
+        prev
+        next
+        totalCount
+      }
+    }
+  }
+`;
+
+fetch('https://api.lens.xyz/graphql', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ query })
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
